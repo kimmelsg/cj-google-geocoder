a6437a365c7caf51404725825d87ff6f45fb3ada
Josh Forbes <jforbes@kimmel.com>
2016-11-10 10:18:19 -0500

    Adding a service provider to ease use with laravel.

---
+++ src/GoogleGeocoderServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Spatie\Geocoder;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\ServiceProvider;
+use NavJobs\GoogleGeocoder\GoogleGeocoder;
+
+class GeocoderServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../../../config/geocoder.php' => config_path('geocoder.php'),
+        ], 'config');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../../../config/geocoder.php', 'geocoder');
+
+        $this->app->bind(GoogleGeocoder::class, function ($app) {
+            $client = new Client();
+            return new GoogleGeocoder(
+                $client,
+                config('geocoder.key'),
+                config('geocoder.language'),
+                config('geocoder.region')
+            );
+        });
+    }
+}
